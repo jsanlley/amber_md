cb9e0884916dc51092727ddf294ab934b4ee7cb1
jsanlley <javiersanlley@gmail.com>
2025-12-11 11:02:40 -0800

    deleted test files

--- amber_md_scripts/test/min/run_min.sh
+++
@@ -1,8 +0,0 @@
-    #!/bin/bash
-    module load amber
-
-    pmemd.cuda -O -i min1.mdin -o monomer_min1.mdout -p monomer_solvated.prmtop -c monomer_solvated.inpcrd -r monomer_min1.rst -ref monomer_solvated.inpcrd -inf monomer_min1.info
-    pmemd.cuda -O -i min2.mdin -o monomer_min2.mdout -p monomer_solvated.prmtop -c monomer_min1.rst -r monomer_min2.rst -ref monomer_min1.rst -inf monomer_min2.info 
-    pmemd.cuda -O -i min3.mdin -o monomer_min3.mdout -p monomer_solvated.prmtop -c monomer_min2.rst -r monomer_min3.rst -ref monomer_min2.rst -inf monomer_min3.info
-    pmemd.cuda -O -i min4.mdin -o monomer_min4.mdout -p monomer_solvated.prmtop -c monomer_min3.rst -r monomer_min4.rst -ref monomer_min3.rst -inf monomer_min4.info
-    pmemd.cuda -O -i min5.mdin -o monomer_min5.mdout -p monomer_solvated.prmtop -c monomer_min4.rst -r monomer_min5.rst -ref monomer_min4.rst -inf monomer_min5.info
--- amber_md_scripts/test/prod/1/run_prod.sh
+++
@@ -1,7 +0,0 @@
-    #!/bin/bash
-    module load amber
-
-    pmemd.cuda -O -i prod.mdin -o monomer_prod1.mdout -p monomer_solvated.prmtop -c monomer_equil.rst -r monomer_prod1.rst -ref monomer_equil.rst -inf monomer_prod1.info -x monomer_prod1.nc
-    #pmemd.cuda -O -i prod.mdin -o monomer_prod2.mdout -p monomer_solvated.prmtop -c monomer_prod1.rst -r monomer_prod2.rst -ref monomer_prod1.rst -inf monomer_prod2.info -x monomer_prod2.nc
-    #pmemd.cuda -O -i prod.mdin -o monomer_prod3.mdout -p monomer_solvated.prmtop -c monomer_prod2.rst -r monomer_prod3.rst -ref monomer_prod2.rst -inf monomer_prod3.info -x monomer_prod3.nc
-    #pmemd.cuda -O -i prod.mdin -o monomer_prod4.mdout -p monomer_solvated.prmtop -c monomer_prod3.rst -r monomer_prod4.rst -ref monomer_prod3.rst -inf monomer_prod4.info -x monomer_prod4.nc
