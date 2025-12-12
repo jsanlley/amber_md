d563dc94c6933759f68002b97aa9cda55c736726
jsanlley <javiersanlley@gmail.com>
2025-12-11 17:08:04 -0800

    prepare_pdb script

--- maestro-protein-prep/prepare_pdb.py
+++ maestro-protein-prep/prepare_pdb.py
@@ -1,64 +1,40 @@
-"""
-Copy PDB file into the build/pdb file and run the following commands (from the maestro bash scripts)
-"""
+import sys
+from schrodinger import structure
+from schrodinger.protein import seqres # seqres is a dictionary mapping chain names to residue names
+from schrodinger.structutils import build
 
-import os
-import shutil
-import glob
-import subprocess
+def main():
 
-def copy_file(src, dest, pattern):
-    """
-    Copies a file based on pattern (*pdb)
+    if len(sys.argv) < 1:
+        print("Usage: python prepare_pdb.py input.pdb")
+        sys.exit(1)
 
-    Args:
-        src (str): Source directory file files
-        dest (str): Destination directory file files
-        pattern (str): Pattern to match (e.g. '*pdb')
-    """
+    input_file = sys.argv[1]
 
-    #Create list of matches
-    matched_files = glob.glob(os.path.join(src,pattern))
+    # read in pdb file
+    st = structure.StructureReader.read(input_file)
+    print("structure")
+    print(st)
 
-    if not matched_files:
-        print("File not found")
-        return
+    print("sequence")
+    print(seqres.get_seqres(st))
 
-    # Iterate over match list and copy to pdb file
-    for match_path in matched_files:
-        try:
-            name = os.path.basename(match_path)
-            print(name)
+    # delete solvent fragments
+    solvents = ["DMS","GOL"]
 
-            dest_path = os.path.join(dest, name)
 
-            shutil.copy2(match_path, dest_path)
-            print(f"Copied file from {src} to {dest}")
-        except FileNotFoundError:
-            print(f"Source file not found: {src}")
-        except Exception as e:
-            print(f"Error copying file: {e}")
 
-def run_maestro_command(*args):
-    """
-    Create a maestro .mae file from the .pdb file in the directory
-    """
-    script_path = "/maestro_proteinprep.sh"
-    command = ["bash",script_path]+ list(args)
-    command_test = ['. ~/scripts/maestro_proteinprep.sh', '8dz0']
+    # reverse mutation in C145A to alanine
+    res, *_ = st.residue
+    print(res,*_)
+    print(res.getAtomIndices())
 
-    result = subprocess.run(command_test, shell=True, capture_output=True)
-    print(result)
-    return result
+    # delete solvent fragments (non canonical)
 
+    # write modified structure to disk (as a mae file)
+    modified_fname = f"{input_file[:-4]}.mae"
+    with structure.StructureWriter(f"maestro/{modified_fname}") as writer:
+        writer.append(st)
 
 if __name__ == "__main__":
-
-    #define working and output directories
-    system_dir = os.path.abspath('mpro_tools/tests')
-    output_dir = os.path.join(system_dir,'system/build/pdb')
-    print(system_dir)
-    print(output_dir)
-
-
-
+    main()
