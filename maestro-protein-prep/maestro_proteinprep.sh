e9fa1b8153a121f9e3d1edd53e2b802bf060a86e
jsanlley <javiersanlley@gmail.com>
2025-12-11 17:04:47 -0800

    created maestro prepare pdb script

--- maestro-protein-prep/maestro_proteinprep.sh
+++ maestro-protein-prep/maestro_proteinprep.sh
@@ -6,11 +6,13 @@
 FILE_NAME="$1"
 
 # Convert pdb file to maestro file
-/software/repo/moleculardynamics/schrodinger/2024u1/utilities/structconvert -ipdb ${FILE_NAME}.pdb -omae maestro/${FILE_NAME}.mae
+echo "Convering $1 into .mae file"
+mkdir -p pdb/maestro
+/software/repo/moleculardynamics/schrodinger/2024u1/utilities/structconvert -ipdb pdb/${FILE_NAME}.pdb -omae pdb/maestro/${FILE_NAME}.mae
 
 # Run the protein preparation wizard to prepare protein (wait until its done)
 echo "Running ProteinPrep"
 /software/repo/moleculardynamics/schrodinger/2024u1/utilities/prepwizard maestro/${FILE_NAME}.mae maestro/${FILE_NAME}_prepared.mae \
--fillsidechains -disulfides -assign_all_residues -rehtreat -max_states 1 -epik_pH 7.4 -epik_pHt 0.0 \
+-fillsidechains -assign_all_residues -rehtreat -max_states 1 -epik_pH 7.4 -epik_pHt 0.0 \
 -antibody_cdr_scheme Kabat -samplewater -propka_pH 7.4 -f S-OPLS -rmsd 0.3 -watdist 8 -addOXT \
 -JOBNAME maestro_proteinprep -HOST localhost:4
